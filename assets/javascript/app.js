7e9f2810b6decd60906277c3fd0bd263efc7b383
DaRican <jcastillo8150@gmail.com>
2019-07-16 12:30:12 -0500

    app.js completed without onclick functions'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,41 @@
-$(document).ready(function () {
+/$(document).ready(function () {
 
-    var cartoons = ["The Simpsons", "Darkwing Duck", "Talespin", "Ducktales"];
+var subject = ["mustang", "corvette", "camaro", "wrangler", "f150"];
 
-    function displayCartoonGifs() {
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=DNz9vFegwkYmNtcTUIXy7ZYaJModyCRp&q=" + cartoons + "&limit=25&offset=0&rating=G&lang=en"
+//Display the default buttons on the page
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).then(function (response) {
+for (var i = 0; i < subject.length; i++) {
+    var makeButtons =
+        $('<button').text(subject[i]).addClass('onLoadButtons');
+    makeButtons.attr("car-model", subject[i]);
+    $('#buttons').append(makeButtons);
+};
 
-            var cartoonDiv = $('')
+$(".onLoadButtons").on('click', function () {
+    var carModel = $(this).attr('car-model');
+    var queryURL = ""
 
-        })
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data
+        for (var k = 0; k < results.length; k++) {
+            var picDiv = $('<div>');
+            var rating = results[k].rating;
 
+            var p = $('<p>').text("Rating: " + rating);
+
+            var carPic = $('<img>');
+            carPic.attr("src", results[k].images.fixed_height_still.url);
+            carPic.attr("stateStill", results[k].images.fixed_height_still.url);
+            carPic.attr("stateAnimated", results[k].images.fixed_height.url)
+            carPic.attr("class", "pics");
+        }
+
+    })
+
+})
 
     }
 
