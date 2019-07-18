db0f4f971ec84dd6792a6253ff1456eda3dc0298
DaRican <jcastillo8150@gmail.com>
2019-07-17 19:44:59 -0500

    laptop commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,63 @@
-/$(document).ready(function () {
+$(document).ready(function () {
+    const key = "DNz9vFegwkYmNtcTUIXy7ZYaJModyCRp"
+    var cars = ["mustang", "corvette", "camaro", "wrangler", "f150"];
 
-var subject = ["mustang", "corvette", "camaro", "wrangler", "f150"];
+    //Display the default buttons on the page
 
-//Display the default buttons on the page
+    for (var i = 0; i < cars.length; i++) {
+        var makeButtons =
+            $('<button').text(cars[i]).addClass('onLoadButtons');
+        makeButtons.attr("car-model", subject[i]);
+        $('#buttons').append(makeButtons);
+    };
 
-for (var i = 0; i < subject.length; i++) {
-    var makeButtons =
-        $('<button').text(subject[i]).addClass('onLoadButtons');
-    makeButtons.attr("car-model", subject[i]);
-    $('#buttons').append(makeButtons);
-};
+    $(".onLoadButtons").on('click', function () {
+        var carModel = $(this).attr('car-model');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + carModel + "&api_key=DNz9vFegwkYmNtcTUIXy7ZYaJModyCRp&limit=9";
 
-$(".onLoadButtons").on('click', function () {
-    var carModel = $(this).attr('car-model');
-    var queryURL = ""
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data
+            for (var k = 0; k < results.length; k++) {
+                var picDiv = $('<div>');
+                var rating = results[k].rating;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var results = response.data
-        for (var k = 0; k < results.length; k++) {
-            var picDiv = $('<div>');
-            var rating = results[k].rating;
+                var p = $('<p>').text("Rating: " + rating);
 
-            var p = $('<p>').text("Rating: " + rating);
+                var carPic = $('<img>');
+                carPic.attr("src", results[k].images.fixed_height_still.url);
+                carPic.attr("stateStill", results[k].images.fixed_height_still.url);
+                carPic.attr("stateAnimated", results[k].images.fixed_height.url)
+                carPic.attr("class", "pics");
 
-            var carPic = $('<img>');
-            carPic.attr("src", results[k].images.fixed_height_still.url);
-            carPic.attr("stateStill", results[k].images.fixed_height_still.url);
-            carPic.attr("stateAnimated", results[k].images.fixed_height.url)
-            carPic.attr("class", "pics");
-        }
+                picDiv.prepend(p);
+                picDiv.prepend(carPic);
+                $('#carModel-view').prepend(picDiv);
+            };
+
+        })
+
+    });
+
+    $('#add-carModel').on("click", function () {
+
+        event.preventDefault();
+        var carModelInput = $("#carModel-input").toLowerCase().val().trim();
+
+        var makeButtons = $('<button>').text(carModelInput);
+        makeButtons.attr("car-model", carModelInput);
+        makeButtons.attr("class", "newButton");
+        $('#allButtons').append(makeButtons);
+        $('#carModel-input').empty();
 
     })
 
 })
 
-    }
 
 
 
 
 
-
-})
